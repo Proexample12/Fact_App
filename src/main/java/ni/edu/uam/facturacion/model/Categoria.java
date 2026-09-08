5b9380daa7ca3056e276f45740e6598ae16c3a18
Kenneth Zamora <kmzamora@uamv.edu.ni>
2026-09-08 16:00:47 -0600

    Update Categoria Producto

--- src/main/java/ni/edu/uam/facturacion/model/Categoria.java
+++ src/main/java/ni/edu/uam/facturacion/model/Categoria.java
@@ -1,4 +1,17 @@
 package ni.edu.uam.facturacion.model;
 
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categoria {
+    private Integer id;
+    private String nombre;
+    private Boolean activo;
+
+    @Override
+    public String toString() {
+        return nombre; }
 }
--- src/main/java/ni/edu/uam/facturacion/model/Producto.java
+++ src/main/java/ni/edu/uam/facturacion/model/Producto.java
@@ -1,4 +1,19 @@
 package ni.edu.uam.facturacion.model;
 
+import lombok.*;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Producto {
+    private Integer id;
+    private String codigo;
+    private String nombre;
+    private Categoria categoria;
+    private BigDecimal precioVenta;
+    private int existencia;
+    private String rutaImagen;
+    private boolean activo;
 }
